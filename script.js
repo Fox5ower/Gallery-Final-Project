d49931896600f756faf4e8151f9e8abb076d5a88
Fox5ower <max7im13@tut.by>
2019-11-15 09:35:44 +0300

    Some fixes, new features

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const progress = document.getElementById("progress-bar");
   document.body.addEventListener(eventName, preventDefaults, false);
 });
 
-//border color changer
 ["dragenter", "dragover"].forEach(eventName => {
   dropArea.addEventListener(eventName, highlight, false);
 });
@@ -29,77 +28,39 @@ function unhighlight() {
   dropArea.classList.remove("highlight");
 }
 
-function hideProgress() {
-  progress.classList.remove("progShow");
-}
-
-function showProgress() {
-  progress.classList.add("progShow");
-}
-
 function handleDrop(e) {
   var dt = e.dataTransfer;
   var files = dt.files;
   handleFiles(files);
 }
 
-let uploadProgress = [];
-let progressBar = document.getElementById("progress-bar");
-
-function initializeProgress(numFiles) {
-  progressBar.value = 0;
-  uploadProgress = [];
-
-  for (let i = numFiles; i > 0; i--) {
-    uploadProgress.push(0);
-  }
-}
-
-function updateProgress(fileNumber, percent) {
-  uploadProgress[fileNumber] = percent;
-  let total =
-    uploadProgress.reduce((tot, curr) => tot + curr, 0) / uploadProgress.length;
-  progressBar.value = total;
-}
-
 function handleFiles(files) {
   files = [...files];
-  initializeProgress(files.length);
-  files.forEach(uploadFile);
   files.forEach(previewFile);
 }
 
+let counter = 0;
+
 function previewFile(file) {
+  counter++;
   let reader = new FileReader();
   reader.readAsDataURL(file);
   reader.onloadend = function() {
     let img = document.createElement("img");
     img.src = reader.result;
+    localStorage.setItem(counter, reader.result);
     document.getElementById("gallery").appendChild(img);
   };
 }
 
-function uploadFile(file, i) {
-  var url = "https://api.cloudinary.com/v1_1/dkirggnhn/image/upload"; // API upload link
-  var xhr = new XMLHttpRequest();
-  var formData = new FormData();
-  xhr.open("POST", url, true);
-  xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-
-  xhr.upload.addEventListener("progress", function(e) {
-    showProgress();
-    updateProgress(i, (e.loaded * 100.0) / e.total || 100); // Set progress bar value
-  });
-
-  xhr.addEventListener("readystatechange", function(e) {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      updateProgress(i, 100);
-      hideProgress();
-    } else if (xhr.readyState == 4 && xhr.status != 200) {
-      alert("An error occured");
-    }
-  });
-  formData.append("upload_preset", "hpkxi2js"); // API preset name
-  formData.append("file", file);
-  xhr.send(formData);
-}
+window.onload = function() {
+  counter = document.cookie;
+  for (let i = 1; i <= localStorage.length; i++) {
+    let img = document.createElement("img");
+    img.src = localStorage.getItem(i);
+    document.getElementById("gallery").appendChild(img);
+  }
+};
+window.beforeunload = function() {
+  document.cookie = counter;
+};
